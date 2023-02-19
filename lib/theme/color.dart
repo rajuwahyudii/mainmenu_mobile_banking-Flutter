e0b630a0b7fc5fe52e31f71798d2d584138b938e
Rwhytm <rajuwahyudi842@gmail.com>
2023-02-19 11:36:42 +0700

    set color for app in file color.dart and forlder theme

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 
-const Color primaryColor = Colors.blue;
+//color for background
+const Color greyBackgroundColor = Color(0xffE0DCDC);
+
+//color for background button
+const Color greyButtonColor = Color(0xffD9D9D9);
+
+//color for text
+const Color darkTextColor = Color(0xff353B43);
+const Color lightTextColor = Color(0xffFFFFFF);
+const Color cashOutTextColor = Color(0xffCD4431);
+const Color cashInTextColor = Color(0xff3ECD31);
+
+//color for icon
+const Color greyIconColor = Color(0xff938E8E);
+const Color darkIconColor = Color(0xff000000);
